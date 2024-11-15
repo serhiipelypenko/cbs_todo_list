44e5ff9817130bf0c7f7be2ee264b2be86ecee1a
serhiipelypenko <sergey.pelipenko@triogeeks.com>
2024-11-15 15:24:23 +0200

    Some features

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const taskForm = document.getElementById("task-form");
-    const taskInput = document.getElementById("task-input");
-    const taskList = document.getElementById("task-list");
+let tasks = [
+    {
+        title: 'Купити хліб',
+        done: true,
+    },
+    {
+        title: 'Забрати дитину з садочка',
+        done: false,
+    },
+    {
+        title: 'Накормити черепаху',
+        done: false,
+    }
+];
 
-    taskForm.addEventListener("submit", (e) => {
-        e.preventDefault();
+const taskForm = document.getElementById("task-form");
+const taskInput = document.getElementById("task-input");
+const taskList = document.getElementById("task-list");
 
-        const taskText = taskInput.value;
-        if (taskText.trim() === "") return;
 
-        const li = document.createElement("li");
-        li.textContent = taskText;
+for (let i = 0; i < tasks.length; i++) {
+    let li = createListItem(tasks[i].title);
+    if(tasks[i].done){
+        li.classList.add("completed");
+    }
+    taskList.appendChild(li);
+    taskInput.value = "";
+}
 
-        const deleteBtn = document.createElement("button");
-        deleteBtn.textContent = "Delete";
-        deleteBtn.addEventListener("click", () => li.remove());
-
-        li.addEventListener("click", () => li.classList.toggle("completed"));
-
-        li.appendChild(deleteBtn);
-        taskList.appendChild(li);
-
-        taskInput.value = "";
-    });
+taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let li = createListItem(taskInput.value);
+    taskList.appendChild(li);
+    taskInput.value = "";
 });
+
+function createListItem(title) {
+    if (title.trim() === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = title;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.addEventListener("click", () => li.remove());
+
+    li.addEventListener("click", () => li.classList.toggle("completed"));
+    li.appendChild(deleteBtn);
+
+    return li;
+}
